d7cc9e6ac20d801361d3ac1738f759644694bfac
narol01 <kmingogod@gmail.com>
2024-04-02 10:03:28 +0200

    change

--- src/main/java/ait/imagga2/dto/BackFrontImageColorsDto.java
+++ src/main/java/ait/imagga2/dto/BackFrontImageColorsDto.java
@@ -2,10 +2,15 @@ package ait.imagga2.dto;
 import lombok.Getter;
 import lombok.ToString;
 
-@ToString
+
 @Getter
 public class BackFrontImageColorsDto {
     private String closest_palette_color;
     private String closest_palette_color_parent;
     private double percent;
+
+    @Override
+    public String toString() {
+        return  closest_palette_color + "\t\t" + closest_palette_color_parent + "\t\t" + percent;
+    }
 }
